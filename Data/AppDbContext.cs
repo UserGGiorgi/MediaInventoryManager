88d23ad87c1bc74e4df0ee71eaca0a1316e807ea
gio <gio@GIOPC>
2026-04-29 15:57:31 +0400

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using MediaInventoryManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaInventoryManager.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
